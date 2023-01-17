3d22e71ea536e5584e00fbb9cbe707919c2e7a64
Shivani21-hude <shivani21hude@gmail.com>
2023-01-17 21:38:36 +0530

    [Shivani]-Test case 1,maximum value at 1st position

---
+++ Generics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Generics
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("To find maximum value betwwen 3 inputs using Generics \n");
+
+           Console.WriteLine( "Maximnum value is : "+TestCase1.MaximumIntergerNumber(5,2,1));
+            
+        }
+    }
+}
