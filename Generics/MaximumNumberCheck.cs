825b4535ba199abcf3513e0d0d4eda1b5dc8801d
Shivani21-hude <shivani21hude@gmail.com>
2023-01-18 21:28:39 +0530

    [Shivani]-Refactor all 3 to one Generic method and find maximum

--- Generics/MaximumNumberCheck.cs
+++ Generics/MaximumNumberCheck.cs
@@ -8,7 +8,7 @@ namespace Generics
 {
     internal class MaximumNumberCheck
     {
-        public static int MaximumIntergerNumber(int firstvalue, int secondvalue, int thirdvalue)
+        public static T Maximum<T>(T firstvalue, T secondvalue, T thirdvalue) where T : IComparable
         {
             if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
             {
@@ -24,48 +24,7 @@ namespace Generics
             }
             throw new Exception("firstNumber,secondNumber and thirdNumber are same");
         }
-        //find maximum float value
-        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
-        {
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
-            {
-                return firstValue;
-            }
-            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
-            {
-                return secondValue;
-            }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
-            {
-                return thirdValue;
-            }
-            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
-        }
-        //find maximum string value
-        public static string MaximumStringNumber(string firstString, string secondString, string thirdString)
-        {
-            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
-                firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
-                firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
-            {
-                return firstString;
-            }
-
-            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
-                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
-                secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
-            {
-                return secondString;
-            }
-
-            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
-                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
-                thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
-            {
-                return thirdString;
-            }
-
-            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
-        }
+       
+       
     }
 }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -10,9 +10,9 @@ namespace Generics
             Console.WriteLine("To find maximum value betwwen 3 inputs using Generics \n");
 
          
-            Console.WriteLine("Maximnum int  value is : " + MaximumNumberCheck.MaximumIntergerNumber(12, 22, 20));
-            Console.WriteLine("Maximnum float value is : " + MaximumNumberCheck.MaximumFloatNumber(44.2,42.9, 2.0));
-            Console.WriteLine("Maximnum string value is : " + MaximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana"));
+            Console.WriteLine("Maximnum int  value is : " + MaximumNumberCheck.Maximum(12, 22, 20));
+            Console.WriteLine("Maximnum float value is : " + MaximumNumberCheck.Maximum(44.2,42.9, 2.0));
+            Console.WriteLine("Maximnum string value is : " + MaximumNumberCheck.Maximum("Apple", "Peach", "Banana"));
 
         }
     }
