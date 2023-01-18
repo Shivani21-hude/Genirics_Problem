0e7b3f9a3d61f3bac6ccf63d40be01165b24ed6b
Shivani21-hude <shivani21hude@gmail.com>
2023-01-18 22:05:41 +0530

    [Shivani]-Extend max method to take more than tree values

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 namespace Generics
 {
@@ -9,11 +10,20 @@ namespace Generics
         {
             Console.WriteLine("To find maximum value betwwen 3 inputs using Generics \n");
 
-         
-            Console.WriteLine("Maximnum int  value is : " + MaximumNumberCheck<int>.Maximum(12, 22, 20));
-            Console.WriteLine("Maximnum float value is : " + MaximumNumberCheck<float>.Maximum(44.2f,42.9f, 2.0f));
-            Console.WriteLine("Maximnum string value is : " + MaximumNumberCheck<string>.Maximum("Apple", "Peach", "Banana"));
 
+            //Console.WriteLine("Maximnum int  value is : " + MaximumNumberCheck<int>.Maximum(12, 22, 20));
+            //Console.WriteLine("Maximnum float value is : " + MaximumNumberCheck<float>.Maximum(44.2f,42.9f, 2.0f));
+            //Console.WriteLine("Maximnum string value is : " + MaximumNumberCheck<string>.Maximum("Apple", "Peach", "Banana"));
+
+            int[] intArray = { 22, 76, 34,89,45 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.PrintMaxValue();
+            double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+            string[] stringArray = { "111", "222", "333","446","665" };
+            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+            genericString.PrintMaxValue();
         }
     }
 }
