5c324c665c3ee3fc60cca4b62cd09dd16eef0ced
Shivani21-hude <shivani21hude@gmail.com>
2023-01-18 21:06:15 +0530

    [Shivani]-Find maximum String value

--- Generics/MaximumNumberCheck.cs
+++ Generics/MaximumNumberCheck.cs
@@ -24,6 +24,7 @@ namespace Generics
             }
             throw new Exception("firstNumber,secondNumber and thirdNumber are same");
         }
+        //find maximum float value
         public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
         {
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
@@ -38,6 +39,32 @@ namespace Generics
             {
                 return thirdValue;
             }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
+        //find maximum string value
+        public static string MaximumStringNumber(string firstString, string secondString, string thirdString)
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+            {
+                return firstString;
+            }
+
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+            {
+                return secondString;
+            }
+
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                return thirdString;
+            }
+
             throw new Exception("firstNumber,secondNumber and thirdNumber are same");
         }
     }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -12,6 +12,7 @@ namespace Generics
          
             Console.WriteLine("Maximnum int  value is : " + MaximumNumberCheck.MaximumIntergerNumber(12, 22, 20));
             Console.WriteLine("Maximnum float value is : " + MaximumNumberCheck.MaximumFloatNumber(44.2,42.9, 2.0));
+            Console.WriteLine("Maximnum string value is : " + MaximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana"));
 
         }
     }
